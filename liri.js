869ca4c0f9d9e77cd77c2869662b7c7953d7e74a
Joshua Balao <joshuacbalao@gmail.com>
2020-06-09 21:55:21 -0400

    Add spotify keys to env file nd added more clobal variables to liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,21 @@
+/* The following are the Deoendacies of the Liri App
+    they are also the starting global variables to assist in creating the 
+    data provided to the user's request
+*/
+
+//These are the enviroment vairables
 require("doteenv").config();
+
+var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 
-var spotify = new spotify(keys.spotify)
+var spotify = new Spotify(keys.spotify)
+
+
+//This is the function that will use the BandsinTown to locate the conert
+
+var concertThis = function(artist) {
+    var region = ""
+    var queryUrl = "https://rest.bandsintown.com/artist/" + artist.replace(" ", "+") + "/events?app_id=codingbootcamp"
+}
