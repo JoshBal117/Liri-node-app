337ea5b96d383eb1d9bc4a18e3550fbab3a883d3
Joshua Balao <joshuacbalao@gmail.com>
2020-07-13 18:13:14 -0400

    ensured files are accurate

--- liri.js
+++ liri.js
@@ -1,151 +1,138 @@
-/* The following are the Dependacies of the Liri App
-    they are also the starting global variables to assist in creating the 
-    data provided to the user's request
-*/
+const dotenv = require("dotenv").config();
+var Spotify = require("node-spotify-api")
+const keys = require("./keys.js");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const moment = require("moment")
 
-//These are the enviroment vairables
-require("dotenv").config();
-//this the spotify api key
-var Spotify = require("node-spotify-api");
-var axios = require("axios")
-var keys = require("./keys.js")
-var request = require("request")
-var fs = require("fs")
 var spotify = new Spotify(keys.spotify)
-const dayjs = require("dayjs")
 
-// function cortanaLiri () {
-//     switch (type) {
-//         case "concert-this":
-//             concertThis() 
-//             break;
 
-//         case "spotify-this-song":
-//             spottySong()
-//             break;
-        
-//         case "movie-this":
-//             findMovie()
-//             break;
-        
-//         case "Do-what-it-says":
 
-        
-        
-//         default:
-//             break;
-//     }
-// }
-//
-var type = process.argv[2];
 
-var term = process.argv.slice(3).join(" ");
-//This is the function that will use the BandsinTown to locate the concert
-    
-function concertThis(search) {
-        console.log("Finding your conerts...")
-    //default Foo Fighters
-    if (!search) {
-        search = "Foo+Fighters";
+//=======================================================
+//variable for command 
+// let command = process.argv[2];
+// let secondCom = process.argv[3];
+
+// ********** I ADDED YOUR SWITCH INSIDE OF A FUNCTION, SO WE CAN RE USE AN RE CALL THE SWITCH WHENEVER WE WANT.
+let actionable = function (command, secondCom) {
+
+    switch (command) {
+        case ("concert-this"):
+            concertThis(secondCom);
+            break;
+        case "spotify-this-song":
+            spotifyThis(secondCom);
+            break;
+        case "movie-this":
+            movieThis(secondCom);
+            break;
+        case "do-what-it-says":
+            doWhat();
+            break;
+        default: console.log("Try Again")
+            break;
     };
+}
 
-    var queryUrl = "https://rest.bandsintown.com/artist/" + search + "/events?app_id=codingbootcamp"
-    console.log(queryUrl);
 
-    axios
-        .get(queryUrl)
-        .then(function(res) {
-        console.log("========NEW SHOW LIST========")
-        console.log(`Catch ${search} at: \n`);
-        for (let i = 0; i < res.data.length; i++) {
-            var venue = res.data[i].venue.name;
-            var location = res.data[i].venue.city + ", " +res.data[i].venue.region;
-            var date = dayjs(res.data[i].datetime).format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
-            console.log(`${venue} in ${location} on ${date}`);
+function spotifyThis(song) {
+    spotify.search({ type: "track", query: song, limit: 1 }, function (err, data) {
+        if (!err) {
+            for (let i = 0; i < data.tracks.items.length; i++) {
+                let songData = data.tracks.items[i];
+
+                //artist
+                console.log("Artist: " + songData.artists[0].name);
+
+                //console log song name
+                console.log("Song: " + songData.name);
+
+                //console log preview link
+                console.log("Preview: " + songData.preview_url);
+
+                //console log album name
+                console.log("Album: " + songData.album.name);
             }
-            console.log("\n--------------------------------");
-    });
-    
-}
-
-function spottySong(search) {
-    console.log("Finding your song ,,,")
-    if(!search) {
-        search = "The Sign, Ace of Base";
-    }
-
-    sprotify.search({
-            type: 'track',
-            query: search 
-    }, function(err, data) {
-        if(err) {
-                return console.log('Errpr occurred: ' + err);
+        } else {
+            console.log("err");
         }
-        var track = data.tracks.items[0].name; 
-        var artist = data.tracks.items[0].album.artists[0].name; 
-        var album = data.tracks.items[0].album.name; 
-        var preview = data.tracks.items[0].preview_url; 
-        console.log("=========NEW SONG=========\n");
-        console.log(`Check out "${track}" off of the album '${album}' by '${artist}' here: ${preview}`);
-        console.log("\n==========================");
     });
 }
 
-function findMovies(search) {
-    console.log("Finding your movie...");
+function movieThis(secondCom) {
+    let queryUrl = "http://www.omdbapi.com/?t=" + secondCom + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMdB Rating: " + response.data.imdbRating);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Rotten Tomatoes URL: " + response.data.tomatoURL);
+        })
+        .catch(function (err) {
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(err.response.data);
+                console.log("---------------Status---------------");
+                console.log(err.response.status);
+                console.log("---------------Status---------------");
+                console.log(err.response.headers);
+            } else if (err.request) {
+                // The request was made but no response was received
+                // `err.request` is an object that comes back with details pertaining to the err that occurred.
+                console.log(err.request);
+            } else {
+                // Something happened in setting up the request that triggered an err
+                console.log("err", err.message);
+            }
+            console.log(err.config);
+        });
 
-    if (!search) {
-        search = "Mr. Nobody";
-    };
-
-    var queryURL = "https://www.omdbapi.com/?apikey=trilogy&t=" + search;
-    
-    axios
-    .get(queryUrl)
-    .then(function(res) {
-        var title = res.data.Title;
-        var year = res.data.Year;
-        var imdb = res.data.Ratings[0].Value;
-        var rotten = res.data.Ratings[1].Value;
-        var country = res.data.Country;
-        var language = res.data.Language;
-        var plot = res.data.Plot;
-        var actors = res.data.Actors;
-        console.log("=========NEW MOVIE=========\n");
-        console.log(`${title} (${year}): ${plot}. \nThe film was produced in ${country} and available in ${language}. Rated ${imdb} on IMDB and ${rotten} on Rotten Tomatoes. \nStarring: ${actors}. `)
-        console.log("\n===========================");
-    });
 }
 
+function concertThis(artist) {
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(function (response) {
 
-if (type === "concert-this") {
-    concertThis(term);
-} else if (type === "spotify-this-song") {
-    spottySong(term)
-} else if(type === "movie-this") {
-    findMovies(term)
-} else if (type === "do-what-it-says") {
-    fs.readFile("randome.txt", "utf8", function(error, data) {
-        if (error) {
-            return console.log(error);
-        };
-    };
-        
-    var dataArr = data.split(",");       
-    var dataAction = dataArr[0];
-    var dataTerm = dataArr[1]
-    if (dataAction === "concert-this") {
-        findConcerts(dataTerm);
-    } else if (dataAction === "spotify-this-song") {
-        findSongs(dataTerm);
-    } else if (dataAction === "movie-this") {
-        findMovies(dataTerm);
-    } else {
-        console.log("Can't understand that file");
-    }
-});
- else {
-console.log("Please enter 'concert-this', 'spotify-this-song', 'movie-this', or 'do-what-it-says'");
+        // Saving response into variable 
+        let concerts = response.data;
+
+        // Loop through response data array to obtain specific data from bands in town api
+        for (let i = 0; i < concerts.length; i++) {
+
+            // Displaying venue info
+            console.log("Venue: " + concerts[i].venue.name);
+            console.log("City: " + concerts[i].venue.city);
+            // Using moment.js to convert date of event to a more user-friendly format
+            console.log("Event Date: " + moment(concerts[i].datetime).format("MM/DD/YYYY"));
+
+        }
+
+    })
+        .catch((error) => {
+            if (error) {
+                console.log(error)
+            }
+        })
 }
 
-   
+function doWhat() {
+    fs.readFile('random.txt', "utf8", function (error, data) {
+        let randomTxt = data.split(',');
+        // AFTER THE FILE IS READ AND SPLIT, WE'RE GOING TO CALL THE FUNCTION THAT CONTAINS THE SWITCH, AND WE'RE GOING TO GIVE
+        // THE ELEMENTS IN THE TXT FILES AS ARGUMENTS, WHICH WILL RUN THE SWITCH. BASICALLY randomTxt[0] IS ASSIGNED IN THE SWITCH TO command 
+        // AND randomTxt[1] is assigned to secondCom and the switch will run with the proper parameters.
+        actionable(randomTxt[0], randomTxt[1]);
+    })
+};
+
+actionable(process.argv[2], process.argv[3]);
